9b2761ae18d5363c38c20103b1aaeb7824c55185
Pablo Vazquez Diaz <pablo.vaz.diaz@gmail.com>
2019-02-07 10:49:17 +0100

    Created UserModel

---
+++ PMMGroupApp/App1/App1/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Models
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public List<Location> Locations { get; set; }
+
+
+    }
+}
